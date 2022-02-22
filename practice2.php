59937bacc188e3864ab8ed47485340e0baa2ac5a
k-takahashi <watoku58@gmail.com>
2022-02-22 21:35:15 +0000

    課題の提出

--- practice2.php
+++ practice2.php
@@ -1,29 +1,33 @@
 <?php
-
+//課題1
 $name = "高";
 if ($name == "高橋"){
- echo "私は高橋です";
+ echo "私は高橋です\n";
 }else{
- echo "高橋ではありません";
+ echo "高橋ではありません\n";
 }
 
+//課題2
 $total = 0;
-echo $total;
-for($i = 0; $i <= 10000; $i++){
+for($i = 1; $i <= 10000; $i++){
     $total += $i;
 }
 echo $total;
+echo "\n";
 
+//課題3
 $fruits = array("apple", "melon", "strawberry", "peach", "grape");
 foreach($fruits as $fruits){
     echo $fruits;
     echo "\n";
 }
 
+//課題4
 $start = 1;
 $end = 100;
 for($i = $start; $i <= $end; $i++){
   if($i % 5 == 0){
     echo $i;
+    echo "\n";
   }
 }
